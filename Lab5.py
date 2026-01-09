c9a07ab93c4bba8c35bd913ad5540a8a716a9acd
mariahatz1585-ai <maria.hatz.1585@gmail.com>
2026-01-09 09:00:29 -0500

    lab 5 maria hatzipetros

--- Lab5.py
+++ Lab5.py
@@ -5,23 +5,51 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    result = ""
+    result += ("*" * n)
+    result += "\n"
+    for i in range(n-2):
+        result += ( "*" + (n-2) * " " + "*")
+        result += "\n"
+    if not(n == 1):
+        result += ("*" * n)
+    return result.rstrip()
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    result = ""
+    for i in range(n):
+        count = 1
+        for k in range( i + 1):
+            result += str(count)
+            count += 1
+        result += "\n"
+    return result.strip()
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
-def sum_of_natural_numbers(n):
-    return ""
-
 # Example for n = 4:
+#    *def sum_of_natural_numbers(n):
+def sum_of_natural_numbers(n):
+    result = 0
+    count = 1
+    while (n >= count):
+            result += count
+            count += 1
+    return result
 #    *
 #   ***
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    result = ""
+    result += (n - 1) * " "
+    result += "*"
+    result += "\n"
+    for i in range(n - 1):
+        result += " " * (n - i -2)
+        result += "*" * ((i + 1) * 2 + 1)
+        result += "\n"
+    return result.rstrip()
